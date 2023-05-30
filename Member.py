4a4f59505f4c5807f962c2ac539cda9a46f106f3
lattecoff <atkbvs@gmail.com>
2023-05-30 20:55:18 +0300

    Added classws Person and Member.

---
+++ Member.py
@@ -0,0 +1,22 @@
+class Member:
+	def __init__(self, 		education="", direct="", company="", department="", position="", country="", city="", address="", phone="", email="", member="", date_request="", date_answer="", theme="", thesis="", date_arriving="", hotel="", date_departure=""):
+		self.education = education
+		self.direct = direct
+		self.company = company
+		self.department = department
+		self.position = position
+		self.country = country
+		self.city = city
+		self.address = address
+		self.phone = phone
+		self.email = email
+		self.member = member
+		self.date_request = date_request
+		self.date_answer = date_answer
+		self.theme = theme
+		self.thesis = thesis
+		self.date_arriving = date_arriving
+		self.hotel = hotel
+		self.date_departure = date_departure
+
+
---
+++ Person.py
@@ -0,0 +1,13 @@
+class Person:
+	def __init__(self, name="", second_name="", birthday="", country="", city="", address="", phone="", email=""):
+		self.name = name
+		self.second_name = second_name
+		self.birthday = birthday
+		self.country = country
+		self.city = city
+		self.address = address
+		self.phone = phone
+		self.email = email
+
+
+		
